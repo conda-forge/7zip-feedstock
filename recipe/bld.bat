591edfe67e2f413a25b4e31208fede898855ce44
Yaroslav Halchenko <debian@onerussian.com>
2020-10-24 14:30:01 -0400

    BF: install all non-plugin dlls alongside with binaries

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,3 +4,9 @@ nmake PLATFORM=x64 NEW_COMPILER=1 MY_DYNAMIC_LINK=1
 copy Bundles\Alone7z\x64\7zr.exe %LIBRARY_PREFIX%\bin\7zr.exe
 copy Bundles\Alone\x64\7za.exe %LIBRARY_PREFIX%\bin\7za.exe
 copy UI\Console\x64\7z.exe %LIBRARY_PREFIX%\bin\7z.exe
+
+copy Bundles\Format7z\x64\7za.dll %LIBRARY_PREFIX%\bin\7za.dll
+copy Bundles\Format7zExtract\x64\7zxa.dll %LIBRARY_PREFIX%\bin\7zxa.dll
+copy Bundles\Format7zExtractR\x64\7zxr.dll %LIBRARY_PREFIX%\bin\7zxr.dll
+copy Bundles\Format7zF\x64\7z.dll %LIBRARY_PREFIX%\bin\7z.dll
+copy Bundles\Format7zR\x64\7zra.dll %LIBRARY_PREFIX%\bin\7zra.dll
