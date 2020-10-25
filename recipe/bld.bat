75cb505c0f85b363383e7db888b743896493bbe9
Yaroslav Halchenko <debian@onerussian.com>
2020-10-25 19:17:26 -0400

    ENH: copy *.lib into \lib\

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,7 @@ nmake PLATFORM=x64 NEW_COMPILER=1 MY_DYNAMIC_LINK=1
 
 copy *\*\x64\7z*.exe %LIBRARY_PREFIX%\bin\
 copy Bundles\Format*\x64\*.dll %LIBRARY_PREFIX%\bin\
+copy Bundles\Format*\x64\*.lib %LIBRARY_PREFIX%\lib\
 
 echo Source
 attrib /s /d
