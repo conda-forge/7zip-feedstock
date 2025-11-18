85d5835ed5f952b00e01e279d19f9e365d76c44e
XhstormR <10527522+XhstormR@users.noreply.github.com>
2025-11-18 11:02:52 +0300

    update

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,13 +1,11 @@
 cd CPP\7zip
 nmake PLATFORM=x64 NEW_COMPILER=1 MY_DYNAMIC_LINK=1
 
-copy Bundles\Alone7z\x64\7zr.exe %LIBRARY_PREFIX%\bin\7zr.exe
-copy Bundles\Alone\x64\7za.exe %LIBRARY_PREFIX%\bin\7za.exe
-copy UI\Console\x64\7z.exe %LIBRARY_PREFIX%\bin\7z.exe
+copy Bundles\Alone7z\x64\7zr.exe %LIBRARY_BIN%\7zr.exe
+copy Bundles\Alone\x64\7za.exe %LIBRARY_BIN%\7za.exe
+copy UI\Console\x64\7z.exe %LIBRARY_BIN%\7z.exe
 
 for /d %%G in (Bundles\Format*) do (
     echo Copying from %%G\x64\
-    ls %%G\x64\
-    copy "%%G\x64\*.lib" "%PREFIX%\Library\lib\" /Y
-    copy "%%G\x64\*.dll" "%PREFIX%\Library\bin\" /Y
+    copy "%%G\x64\*.dll" "%LIBRARY_LIB%\" /Y
 )
