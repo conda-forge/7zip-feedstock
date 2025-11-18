dd30277c1110d47d68ba70da43dccc5c5d3333dc
XhstormR <10527522+XhstormR@users.noreply.github.com>
2025-11-18 11:26:21 +0300

    update build.bat

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,11 +1,13 @@
 cd CPP\7zip
 nmake PLATFORM=x64 NEW_COMPILER=1 MY_DYNAMIC_LINK=1
 
-copy Bundles\Alone7z\x64\7zr.exe %LIBRARY_BIN%\7zr.exe
-copy Bundles\Alone\x64\7za.exe %LIBRARY_BIN%\7za.exe
-copy UI\Console\x64\7z.exe %LIBRARY_BIN%\7z.exe
+robocopy Bundles\Alone7z\x64 "%PREFIX%\bin" 7zr.exe
+robocopy Bundles\Alone2\x64 "%PREFIX%\bin" 7zz.exe
+robocopy Bundles\Alone\x64 "%PREFIX%\bin" 7za.exe
+robocopy UI\Console\x64 "%PREFIX%\bin" 7z.exe
 
 for /d %%G in (Bundles\Format*) do (
-    echo Copying from %%G\x64\
-    copy "%%G\x64\*.dll" "%LIBRARY_LIB%\" /Y
+    echo Copying from %%G\x64
+    ( robocopy "%%G\x64" "%LIBRARY_BIN%" *.dll ) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
+    ( robocopy "%%G\x64" "%LIBRARY_LIB%" *.lib ) ^& IF %ERRORLEVEL% LSS 8 SET ERRORLEVEL = 0
 )
