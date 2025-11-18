8b7b6cd5593f7ea6613864e68d7801e45ebc91b6
XhstormR <10527522+XhstormR@users.noreply.github.com>
2025-11-18 08:38:55 +0300

    convert_feedstock_to_v1_recipe_format

--- recipe/build.bat
+++ recipe/build.bat
@@ -5,5 +5,9 @@ copy Bundles\Alone7z\x64\7zr.exe %LIBRARY_PREFIX%\bin\7zr.exe
 copy Bundles\Alone\x64\7za.exe %LIBRARY_PREFIX%\bin\7za.exe
 copy UI\Console\x64\7z.exe %LIBRARY_PREFIX%\bin\7z.exe
 
-for /d %%G in (dir Bundles\Format*) do copy %%G\x64\*.dll %LIBRARY_PREFIX%\bin\
-for /d %%G in (dir Bundles\Format*) do copy %%G\x64\*.lib %LIBRARY_PREFIX%\lib\
+for /d %%G in (Bundles\Format*) do (
+    echo Copying from %%G\x64\
+    ls %%G\x64\
+    copy "%%G\x64\*.lib" "%PREFIX%\Library\lib\" /Y
+    copy "%%G\x64\*.dll" "%PREFIX%\Library\bin\" /Y
+)
