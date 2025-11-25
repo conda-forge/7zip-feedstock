d0be95f32768628f8b6c3d1876af23c57f56fcc5
XhstormR <10527522+XhstormR@users.noreply.github.com>
2025-11-25 08:23:15 +0300

    polish build.sh (#20)
    * polish build.sh
    
    * fix "install: illegal option -- t"

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,14 +2,12 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-mkdir -p "${PREFIX}/bin"
+make -j${CPU_COUNT} -f makefile.gcc -C CPP/7zip/Bundles/Alone7z/
+make -j${CPU_COUNT} -f makefile.gcc -C CPP/7zip/Bundles/Alone2/
+make -j${CPU_COUNT} -f makefile.gcc -C CPP/7zip/Bundles/Alone/
+make -j${CPU_COUNT} -f makefile.gcc -C CPP/7zip/UI/Console/
 
-make -j${CPU_COUNT} -C CPP/7zip/Bundles/Alone7z/ -f makefile.gcc
-make -j${CPU_COUNT} -C CPP/7zip/Bundles/Alone2/ -f makefile.gcc
-make -j${CPU_COUNT} -C CPP/7zip/Bundles/Alone/ -f makefile.gcc
-make -j${CPU_COUNT} -C CPP/7zip/UI/Console/ -f makefile.gcc
-
-cp CPP/7zip/Bundles/Alone7z/_o/7zr "${PREFIX}/bin"
-cp CPP/7zip/Bundles/Alone2/_o/7zz "${PREFIX}/bin"
-cp CPP/7zip/Bundles/Alone/_o/7za "${PREFIX}/bin"
-cp CPP/7zip/UI/Console/_o/7z "${PREFIX}/bin"
+install -D -m0755 -t "${PREFIX}/bin" CPP/7zip/Bundles/Alone7z/_o/7zr
+install -D -m0755 -t "${PREFIX}/bin" CPP/7zip/Bundles/Alone2/_o/7zz
+install -D -m0755 -t "${PREFIX}/bin" CPP/7zip/Bundles/Alone/_o/7za
+install -D -m0755 -t "${PREFIX}/bin" CPP/7zip/UI/Console/_o/7z
